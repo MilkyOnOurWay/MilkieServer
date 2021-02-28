00e88fee0cf6009b1f42b1bca0cdabd64ec098c9
Choi Dain <twa05157@gmail.com>
2021-02-28 22:25:38 +0900

    Fix: 토큰값으로 회원 탈퇴

--- routes/users/index.js
+++ routes/users/index.js
@@ -6,5 +6,6 @@ const jwtMiddlewares = require('../../middlewares/middlewares');
 router.post('/signup', userController.signup);
 router.post('/signin', userController.signin);
 router.put('/nickName', jwtMiddlewares.userJwt, userController.nickNameChange);
-router.delete('/:id', jwtMiddlewares.userJwt, userController.deleteUser);
+router.delete('/userDelete', jwtMiddlewares.userJwt, userController.deleteUser);
+
 module.exports = router;
